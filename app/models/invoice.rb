caa4c720055ee183fd6b704a0167f88253b12e59
toanleviet95 <toanleviet95@gmail.com>
2017-01-15 08:19:45 +0700

    Update source

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,12 @@ class Invoice < ApplicationRecord
   belongs_to :transaction_payment , foreign_key: "transaction_id", class_name: "Transaction"
   belongs_to :food
   belongs_to :user
+
+  def get_invoices_by_user(user_id)
+    Invoice.where(user_id: user_id).order(created_date: :desc)
+  end
+
+  def get_invoice_by_id(id)
+    Invoice.find(id)
+  end
 end
