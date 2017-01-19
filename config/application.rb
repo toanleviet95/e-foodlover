f57827daa1714cc54f3752befaa030bdf0c4cad2
toanleviet95 <toanleviet95@gmail.com>
2017-01-19 23:14:03 +0700

    Update

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Foodlover
   class Application < Rails::Application
     config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w(ckeditor/* )
+    config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
